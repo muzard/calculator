4690dc982e3ab259b44291a0679472ed72438ba1
muzard <arttu.kokki@gmail.com>
2022-09-24 17:54:33 +0300

    make button functions

--- script.js
+++ script.js
@@ -28,56 +28,120 @@ function operate(a, operator, b) {
 }
 
 const one = document.getElementById("one")
-one.addEventListener("click", () => alert("one"))
+one.addEventListener("click", () => addNum(1))
 
 const two = document.getElementById("two")
-two.addEventListener("click", () => alert("two"))
+two.addEventListener("click", () => addNum(2))
 
 const three = document.getElementById("three")
-three.addEventListener("click", () => alert("three"))
+three.addEventListener("click", () => addNum(3))
 
 const zero = document.getElementById("zero")
-zero.addEventListener("click", () => alert("zero"))
+zero.addEventListener("click", () => addNum(0))
 
 const plus = document.getElementById("plus")
-plus.addEventListener("click", () => alert("plus"))
+plus.addEventListener("click", () => sumNums())
 
 const minus = document.getElementById("minus")
 minus.addEventListener("click", () => alert("minus"))
 
 
 const four = document.getElementById("four")
-four.addEventListener("click", () => alert("four"))
+four.addEventListener("click", () => addNum(4))
 
 const five = document.getElementById("five")
-five.addEventListener("click", () => alert("five"))
+five.addEventListener("click", () => addNum(5))
 
 const six = document.getElementById("six")
-six.addEventListener("click", () => alert("six"))
+six.addEventListener("click", () => addNum(6))
 
 const delet = document.getElementById("delete")
-delet.addEventListener("click", () => alert("delete"))
+delet.addEventListener("click", () => deleteNum())
 
 const mul = document.getElementById("mul")
-mul.addEventListener("click", () => alert("mul"))
+mul.addEventListener("click", () => multiplyNums())
 
 const div = document.getElementById("div")
 div.addEventListener("click", () => alert("div"))
 
 const seven = document.getElementById("seven")
-seven.addEventListener("click", () => alert("seven"))
+seven.addEventListener("click", () => addNum(7))
 
 const eight = document.getElementById("eight")
-eight.addEventListener("click", () => alert("eight"))
+eight.addEventListener("click", () => addNum(8))
 
 const nine = document.getElementById("nine")
-nine.addEventListener("click", () => alert("nine"))
+nine.addEventListener("click", () => addNum(9))
 
 const clear = document.getElementById("clear")
-clear.addEventListener("click", () => alert("clear"))
+clear.addEventListener("click", () => clearNums())
 
 const comma = document.getElementById("comma")
-comma.addEventListener("click", () => alert("comma"))
+comma.addEventListener("click", () => addNum("."))
 
 const eq = document.getElementById("eq")
-eq.addEventListener("click", () => alert("eq")) 
+eq.addEventListener("click", () => equals()) 
+
+let listOfNums = [];
+
+const currentScreen = document.getElementById("calcText")
+let hiddenNum = null;
+let currentNum = 0;
+let operator;
+
+
+// fix this
+function listToNum() {
+    if ("." in listOfNums) {
+        return parseFloat(listOfNums.join(""))
+    }
+    return parseInt(listOfNums.join(""), 10)
+}
+
+function addNum(num) {
+    listOfNums.push(num)
+    currentNum = listToNum()
+    currentScreen.textContent = currentNum
+}
+
+function deleteNum() {
+    listOfNums.pop()
+    currentNum = listToNum()
+    currentScreen.textContent = currentNum
+}
+
+function clearNums() {
+    hiddenNum = null
+    listOfNums = []
+    currentScreen.textContent = 0
+}
+
+function sumNums() {
+    if (hiddenNum !== null) {
+        equals()
+    }
+
+    listOfNums = []
+    hiddenNum = currentNum
+    currentScreen.textContent = 0;
+    currentNum = 0;
+    operator = "+"
+}
+
+function multiplyNums() {
+    if (hiddenNum !== null) {
+        equals()
+    }
+
+    listOfNums = []
+    hiddenNum = currentNum
+    currentScreen.textContent = 0;
+    currentNum = 0;
+    operator = "*"
+}
+
+function equals() {
+    currentNum = operate(hiddenNum, operator, currentNum)
+    hiddenNum = null
+    currentScreen.textContent = currentNum
+}
